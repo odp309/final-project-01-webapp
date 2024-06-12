546b884f03591df9fff19e62595034979fd2ced7
tryaspane <trypane98@gmail.com>
2024-06-12 12:44:49 +0700

    feat : add feature updateStock on BalanceStock

--- src/app/core/services/datatable/balance/balance.service.ts
+++ src/app/core/services/datatable/balance/balance.service.ts
@@ -1,10 +1,11 @@
-import { HttpClient, HttpParams, HttpRequest } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BalanceTable } from '../../../dto/datatable/balanceTable.dto';
 import { Observable } from 'rxjs';
 import { env } from '../../../../env';
 
 const getBalanceApi = env.base_url + '/api/v1/private/branch-reserve/get';
+const postAddStockApi = env.base_url + '/api/v1/private/branch-reserve/add-stock';
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +14,14 @@ export class BalanceService {
 
   constructor( private http:HttpClient) {}
 
-  LoadData(branchName: string): Observable<BalanceTable[]> {
-    const payload = { branchName: branchName };
+  LoadData(branchCode: string): Observable<BalanceTable[]> {
+    const payload = { branchCode: branchCode };
     return this.http.post<BalanceTable[]>(getBalanceApi, payload);
+  }
+
+  AddStock(branchCode: string, currencyCode:string, balance:number): Observable<any> {
+    const payload = {branchCode,currencyCode, balance};
+    return this.http.post(postAddStockApi, payload)
   }
 
 }
--- src/app/modules/balances/page/balances/balances.component.ts
+++ src/app/modules/balances/page/balances/balances.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { Config } from 'datatables.net';
-import { AlluserService } from '../../../../core/services/datatable/users/alluser.service';
 import { BalanceTable } from '../../../../core/dto/datatable/balanceTable.dto';
 import { Subject } from 'rxjs';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { StorageService } from '../../../../core/services/storage/storage.service';
 import { JwtDecoderService } from '../../../../core/services/jwt/jwt-decoder.service';
 import { BalanceService } from '../../../../core/services/datatable/balance/balance.service';
+import { Modal } from 'flowbite';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-balances',
   templateUrl: './balances.component.html',
   styleUrl: './balances.component.css'
 })
-export class BalancesComponent {
+export class BalancesComponent implements OnInit, AfterViewInit {
 
   balanceTable: BalanceTable[] = [];
   dtoptions: Config = {};
@@ -21,15 +22,28 @@ export class BalancesComponent {
   role?: any;
   accessToken?: any;
   user?: any;
+  addStockBalanceForm!: FormGroup;
 
   // get today date, delete this if the api response is valid
   updatedAt: Date = new Date();
+
+  @ViewChild('addStockBalanceModal') addStockBalanceModalRef!: ElementRef;
+  addStockBalanceModal!: Modal;
+  selectedCurrency: any
+
+  showAlert: boolean = false;
+  alertType: 'success' | 'error' = 'success';
+  alertTitle: string = '';
+  alertMessage: string = '';
 
   constructor(
     private service: BalanceService,
     private formBuilder: FormBuilder,
     private storageService: StorageService,
     private jwtDecoderService: JwtDecoderService,
+    private jwtService: JwtDecoderService,
+    private balanceService: BalanceService,
+    private router: Router,
   ) {
     this.accessToken = localStorage.getItem('jwtToken')?.toString();
   }
@@ -51,6 +65,10 @@ export class BalancesComponent {
         searchPlaceholder: 'Search Balance',
       },
     },
+    this.addStockBalanceForm = this.formBuilder.group({
+      balance: ['', Validators.required],
+      branchCode: [this.getBranchCode(), Validators.required],
+    }),
       this.loadData();
   }
 
@@ -76,7 +94,82 @@ export class BalancesComponent {
     fetchData();
 
     // Set an interval to fetch data periodically (e.g., every 5 seconds)
-    setInterval(fetchData, 5000); // Adjust the interval as needed
+    // setInterval(fetchData, 5000); // Adjust the interval as needed
   }
 
+  private getBranchCode(): string {
+    const token = this.storageService.getToken();
+    const decodedToken: any = this.jwtService.decodeToken(token);
+    return decodedToken.branchCode;
+  }
+
+  ngAfterViewInit(): void {
+    // Initialize modals
+      this.initModals();
+  }
+
+  showAlertMessage(type: 'success' | 'error', message: string): void {
+    this.showAlert = true;
+    this.alertType = type;
+    this.alertMessage = message;
+  }
+
+  private initModals(): void {
+    console.log('Initializing modals...');
+
+    if (this.addStockBalanceModalRef) {
+      const addStockBalanceModalElement = this.addStockBalanceModalRef.nativeElement;
+      console.log('Edit User Status Modal Element:', addStockBalanceModalElement);
+      if (addStockBalanceModalElement) {
+        this.addStockBalanceModal = new Modal(addStockBalanceModalElement);
+        console.log('Modals initialized successfully.');
+      } else {
+        console.error('Modal elements are not found in the DOM.');
+      }
+    } else {
+      console.error('Modal ViewChild elements are not available.');
+    }
+  }
+
+  showAddStockBalanceModal(currency: BalanceTable): void {
+    this.selectedCurrency = currency;
+    if (this.addStockBalanceModal) {
+      this.addStockBalanceModal.show();
+    } else {
+      console.error('Add Stock Balance Modal is not initialized.');
+    }
+  }
+
+  hideAddStockBalanceModal(): void {
+    if (this.addStockBalanceModal) {
+      this.addStockBalanceModal.hide();
+    }
+  }
+
+  public addStockBalance():void{
+    console.log("test button save")
+    if (this.selectedCurrency && this.addStockBalanceForm.valid) {
+      console.log(this.selectedCurrency +" and " + this.addStockBalanceForm + " is valid ")
+      const currencyCode= this.selectedCurrency.currencyCode;
+      const balance= this.addStockBalanceForm.get('balance')?.value;
+      const branchCode= this.addStockBalanceForm.get('branchCode')?.value;
+      const payload = { branchCode,currencyCode, balance };
+      console.log('Submitting payload:', payload);
+      this.balanceService.AddStock(branchCode, currencyCode,balance ).subscribe(
+        (response) => {
+          console.log('Stock Balance Added ', response);
+          this.hideAddStockBalanceModal();
+          this.router.navigate(['/balances']).then(() => {
+            window.location.reload();
+          });
+          this.showAlertMessage('success', 'Stock Balance added successfully');
+        },
+        (error) => {
+          console.error('Error update balance', error);
+        }
+      ); 
+    } else {
+      console.log('Form is invalid or selectedCurrency is not set'); // Debugging line
+    }
+  }
 }
